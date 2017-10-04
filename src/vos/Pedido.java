9bbf49d388735fc2f9d9edac6848ff8460faab71
Your Name <you@example.com>
2017-10-04 10:26:09 -0500

    manos a la obra

--- src/vos/Pedido.java
+++ src/vos/Pedido.java
@@ -1,5 +1,37 @@
 package vos;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
 public class Pedido {
+	@JsonProperty(value="id")
+	private Long idPed;
+	@JsonProperty(value="FECHA")
+	private Integer fecha;
+	@JsonProperty(value="CPERSONAS")
+	private Integer cantPersonas;
+	public Pedido(@JsonProperty(value="id") Long pId,@JsonProperty(value="FECHA")Integer pFecha,@JsonProperty(value="CPERSONAS") Integer pCantidadPersonas){
+		super();
+		idPed =pId;
+		fecha = pFecha;
+		cantPersonas = pCantidadPersonas;
+	}
+	public void setId(Long pId){
+		idPed=pId;
+	}
+	public void setFecha(Integer pFecha){
+		fecha = pFecha;
+	}
+	public void setPersonas(Integer pCantidadPersonas){
+		cantPersonas = pCantidadPersonas;
+	}
+	public Integer getFecha(){
+		return fecha;
+	}
+	public Integer getCantPersonas(){
+		return cantPersonas;
+	}
+	public Long getId(){
+		return idPed;
+	}
 
 }
